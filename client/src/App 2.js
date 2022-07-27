e062cfd09248ecbd8e903a5ce9790c5600e3e12f
Brittney <brittneyrichardson8@gmail.com>
2022-07-27 14:33:10 -0500

    added apollo to client

---
+++ client/src/App 2.js
@@ -0,0 +1,15 @@
+import Home from './componets/Home';
+import About from './componets/About'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Home />
+    <About />
+    </>
+  
+  );
+}
+
+export default App;
--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,40 @@
-import Home from './componets/Home';
-import About from './componets/About'
-import Login from './componets/Login';
-import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import Home from "./componets/Home";
+import About from "./componets/About";
+import Login from "./componets/Login";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloClient client={client}>
       <Home />
-    <About />
-    <Login />
-    </>
-  
+      <About />
+      <Login />
+    </ApolloClient>
   );
 }
 
