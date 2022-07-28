0d08b7a0d0c607aa34acb5fa17f38fd732b5ca00
Brittney <brittneyrichardson8@gmail.com>
2022-07-28 08:39:36 -0500

    added header componet

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Home from './componets/Home';
 import About from './componets/About'
 import Login from './componets/Login';
+import Header from './componets/Header';
 import './App.css';
 
 function App() {
   return (
     <>
+    <Header />
       <Home />
     <About />
     <Login />
