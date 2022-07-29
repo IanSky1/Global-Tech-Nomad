4ff47d582f5805ceb28f51dcea273bb37ad70f33
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-07-28 20:01:21 -0500

    added a parent div around the h1 and nav jsx tags to be used with display flexbox in the css.

--- client/src/componets/Header/index.js
+++ client/src/componets/Header/index.js
@@ -3,12 +3,15 @@ import React from "react";
 const Header = () => {
     return (
         <> 
-        <h1>Global Tech Nomad</h1>
-        <nav>
-            <a href="/">Home</a>
-            <a href="/">Sign Up</a>
-            <a href="/">About Us</a>
-        </nav>
+        <div className="header">
+            <h1>Global Tech Nomad</h1>
+            <nav className="navigation">
+                <a href="/">Home</a>
+                <a href="/">Sign Up</a>
+                <a href="/">About Us</a>
+            </nav>
+        </div>
+
         </>
     );
 }
