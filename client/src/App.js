44dede407f49bf12d3cbc480fc21a3593c7cc9b5
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-07-29 02:14:55 -0500

    imported the signup component and rendered it in the app.js jsx return.

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import Home from './componets/Home';
 import About from './componets/About'
 import Login from './componets/Login';
 import Header from './componets/Header';
+import SignUp from './componets/SignUp';
 import './App.css';
 
 function App() {
   return (
     <>
     <Header />
-      <Home />
+    <Home />
     <About />
     <Login />
+    <SignUp/>
     </>
   
   );
