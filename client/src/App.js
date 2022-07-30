0973596f0e313ff8c36b04a0c77b8dbd4b0e0426
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-07-29 22:18:13 -0500

    imported apollo/client and graphql and react router dom and imported them into the app.js file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Home from './componets/Home';
 import About from './componets/About'
 import Login from './componets/Login';
@@ -5,16 +6,62 @@ import Header from './componets/Header';
 import SignUp from './componets/SignUp';
 import './App.css';
 
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
-    <Header />
-    <Home />
-    <About />
-    <Login />
-    <SignUp/>
-    </>
-  
+    <div>
+      <Header />
+      <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <div>
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<SignUp />}
+              />
+
+                <Route element={<About />} />
+
+
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </ApolloProvider>
+    </div>
+
   );
 }
 
