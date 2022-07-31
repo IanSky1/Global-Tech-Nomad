00f6cefdab8cac0ee20628b2a19afe630e4f8c30
Ian <Ian.Mohn1@gmail.com>
2022-07-31 04:10:28 -0500

    updated Header index.js file with import Link and Auth

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,19 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Auth from '../../utils/auth';
 
 const Header = () => {
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+      };
     return (
-        <> 
-        <div className="header">
-            <h1>Global Tech Nomad</h1>
-            <nav className="navigation">
+        <header className="bg-secondary mb-4 py-2 flex-row align-center">
+        <div className="container flex-row justify-space-between-lg justify-center align-center">
+            <Link to="/">
+                <h1>Global Tech Nomad</h1>
+            </Link>
+           
+            <nav className="text-center">
+                
                 <a href="/Home">Home</a>
                 <a href="/SignUp">Sign Up</a>
                 <a href="/About">About Us</a>
                 <a href="/Login">Log In</a>
             </nav>
         </div>
+        </header>
+    
 
-        </>
+        
     );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,2 +1,23 @@
+import React from "react";
+
 import { useQuery } from "@apollo/client";
-import { QUERY_COUNTRIES } from "../utils/queries";
+import { QUERY_CONTINENTS, QUERY_COUNTRIES } from "../utils/queries";
+import Continents from "../components/Continents";
+import Auth from "../utils/auth";
+
+const Home = () => {
+    const { loading, data } = useQuery(QUERY_CONTINENTS, QUERY_COUNTRIES);
+
+    const countries = data?.countries || [];
+    const loggedIn = Auth.loggedIn();
+
+    return (
+        <main>
+          <div className='flex-row justify-space-between'>
+            <div className='col-12 mb-3'>{Continents}</div>
+          </div>
+        </main>
+      );
+    };
+
+export default Home;
