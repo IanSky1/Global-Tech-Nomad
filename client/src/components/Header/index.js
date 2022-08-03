99cc199ff175769942b5962036c8b773632e8865
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-08-02 21:04:52 -0500

    changed the name of componenets because it was misspelled.

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+    <h1>Global Tech Nomad</h1>
+      <nav className="navigation">
+        <Link to='/'>
+            Home
+        </Link>
+
+        <Link to='Login'>
+            Login
+        </Link>
+
+        <Link to='About'>
+            About
+        </Link>
+
+        <Link to='Signup'>
+            SignUp
+        </Link>
+        <Link to='Continents'>
+            Continents
+        </Link>
+
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
